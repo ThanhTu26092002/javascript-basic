a0e2dad62060eb6c7fd4aaf54675a7a514d0f685
ThanhTu26092002 <lethanhtu810@email.com>
2024-02-29 15:46:13 +0700

    bài 10

---
+++ bai10/bai10.js
@@ -0,0 +1,9 @@
+console.log('bai10hello')
+let a = 5;
+let b = '5';
+console.log('so sanh == ', 'a = ',a , 'b = ', b, 'result:', a == b)
+console.log('so sanh ===', 'a = ',a , 'b = ', b, 'result:', a === b)
+
+//empty. underfine. null
+let c = {} ;
+console.log('check value', c)
