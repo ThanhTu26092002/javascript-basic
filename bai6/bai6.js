66898b24337d70e3b19fdc959595535a81b41727
ThanhTu26092002 <lethanhtu810@email.com>
2024-02-29 10:54:19 +0700

    bai 6

---
+++ bai6/bai6.js
@@ -0,0 +1,9 @@
+console.log('hello tui la thanh tu '.toUpperCase());
+let a = 5;
+let b = 6;
+let c = 1.9;
+let d = '5'
+console.log(a+b)
+console.log(a+c)
+console.log(a * d)
+console.log(a === d,  'a:',typeof a,  'd:',typeof d )
