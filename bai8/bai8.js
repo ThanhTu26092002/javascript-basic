e9608ba1932f489d7e4481fa6eae1c7a79cbc9a7
ThanhTu26092002 <lethanhtu810@email.com>
2024-02-29 13:20:21 +0700

    bai8

---
+++ bai8/bai8.js
@@ -0,0 +1,14 @@
+console.log('bai 8');
+//key:value
+let obj = {
+    name: 'thanhtu',
+    address: 'dai loc',
+    email: '',
+    gender:''
+    
+};
+let b = 'name';
+obj[b] = 'Hary';
+obj.b = 'ali'
+console.log('what your name', "My name" , obj['name'], obj)
+console.log('hello' , obj.a())
