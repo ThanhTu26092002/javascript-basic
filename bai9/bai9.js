be348db91cbb1fd38fded983510a83c202d7c014
ThanhTu26092002 <lethanhtu810@email.com>
2024-02-29 13:39:46 +0700

    bai9

---
+++ bai9/bai9.js
@@ -0,0 +1,11 @@
+console.log('bai9')
+let obj = {
+    name: 'ThanhTU',
+    age: '22'
+};
+console.log('check data', obj)
+let b = [
+    'mu','barca','mci'
+]
+console.log('typeof b:', typeof b,b)
+console.log('first item:', b[0], 'second item:', b[1], b)
