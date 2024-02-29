3d96843340f9d66254c816f2a7d8f3f53fa8ebd6
ThanhTu26092002 <lethanhtu810@email.com>
2024-02-29 20:29:44 +0700

    bai12

---
+++ bai12/bai12.js
@@ -0,0 +1,24 @@
+console.log('hellobai12')
+
+
+// let arrTop4 = ['mu', 'mci','liverpool', 'asenal'];
+
+// for(let i = 0; i < arrTop4.length; i++){
+//     console.log('Top:', i + 1 , arrTop4[i])
+// } 
+
+
+//while
+// let i = o;
+// while(true){
+//     console.log("top: ", i + 1, arrTop4[i])
+//     i++;
+// }
+
+let i = 0;
+let flag = false;
+while (!flag) {
+    console.log('check: ',i)
+    i++;
+    if (i === 10) flag=true;
+}
