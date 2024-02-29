7d4f442257a9e37e17f4cf72dbb2e2f3a497fd87
ThanhTu26092002 <lethanhtu810@email.com>
2024-02-29 18:37:27 +0700

    bai11

---
+++ bai11/bai11.js
@@ -0,0 +1,6 @@
+console.log('bai11hello')
+let arr = [ 'mu', 'mci', 'chelsea', 'liverpool']
+for(var i = 0; i <arr.length; i ++){
+    console.log('check variable i=',arr[i])
+}
+console.log('check value i:', i)
