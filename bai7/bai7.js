73b1522ca3849f57a4017b612dd1b40f11ce7a5d
ThanhTu26092002 <lethanhtu810@email.com>
2024-02-29 11:48:50 +0700

    bai 7

---
+++ bai7/bai7.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = a;
+var jsonData = '[{"name":"Tu","class":"12","age":22},{"name":"Ha","class":"13","age":22}]';
+<table>
+<tr>
+    <td>${item.name}</td>
+    <td></td>
+    <td></td>
+</tr>
+</table>
+console.log(`a +b is ${a+b}`)
+console.log(`/hoithanhtu/${a}`)
