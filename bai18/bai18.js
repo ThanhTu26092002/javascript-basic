cb54aca6ceadf9bf85d6bfc2bb8ddb91d03371c4
ThanhTu26092002 <lethanhtu810@email.com>
2024-03-01 11:29:28 +0700

    done

---
+++ bai18/bai18.js
@@ -0,0 +1,15 @@
+console.log('hello 18')
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log('sum', sum(3,10))
+
+//arrow function
+
+
+let sum = ( a,b )=>{
+    return(a+b);
+    
+}
+console.log('sum', sum(3,7))
